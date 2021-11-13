bbc9f5c0488e5cb843ff63a3feb7110936706c2d
kir <kir@your-mail.com>
2021-11-13 09:00:04 +0100

    Simplify query interface, it's much more useful and clean right now

--- src/DataQuery.LanguageExt/SqlQueryHandler.cs
+++ src/DataQuery.LanguageExt/SqlQueryHandler.cs
@@ -1,71 +1,27 @@
 using LanguageExt;
-using LanguageExt.Pipes;
 
 namespace Dataquery.LanguageExt
 {
-    using static Prelude;
-
     public static partial class DataQuery
     {
         /// <summary>
         /// Interface-marker 
         /// </summary>
         public interface ISqlQuery<TResult>
-        { }
-
-        public interface ISqlQueryHandler<TQuery, TResult>
         {
             /// <summary>
-            /// Returns query async effect
+            /// Returns query as async effect
             /// </summary>
-            Aff<RT, TResult> AsAff<RT>(TQuery query) where RT : struct, HasSqlDatabase<RT>;
-
-            /// <summary>
-            /// Returns query async effect as pipe
-            /// </summary>
-            Pipe<RT, TQuery, TResult, Unit> AsPipe<RT>() where RT : struct, HasSqlDatabase<RT>;
-
-            /// <summary>
-            /// Returns query async effect as producer 
-            /// </summary>
-            Producer<RT, TResult, Unit> AsProducer<RT>(TQuery query) where RT : struct, HasSqlDatabase<RT>;
-
-            /// <summary>
-            /// Returns query async effect as consumer 
-            /// </summary>
-            Consumer<RT, TQuery, TResult> AsConsumer<RT>() where RT : struct, HasSqlDatabase<RT>;
+            Aff<RT, TResult> AsAff<RT>() where RT : struct, HasSqlDatabase<RT>;
         }
 
         /// <summary>
-        /// Base class for query handlers, with implemented Pipes methods 
+        /// Base class for query, allows not to put generic constraints
+        /// to the AsAff implementations, thus making code cleaner
         /// </summary>
-        public abstract class SqlQueryHandler<TQuery, TResult> : ISqlQueryHandler<TQuery, TResult>
-            where TQuery : ISqlQuery<TResult>
+        public abstract record SqlQuery<TResult> : ISqlQuery<TResult>
         {
-            public abstract Aff<RT, TResult> AsAff<RT>(TQuery query)
-                where RT : struct, HasSqlDatabase<RT>;
-
-            public Pipe<RT, TQuery, TResult, Unit> AsPipe<RT>()
-                where RT : struct, HasSqlDatabase<RT>
-                =>
-                    from input in Proxy.awaiting<TQuery>()
-                    from result in AsAff<RT>(input)
-                    from _ in Proxy.yield(result)
-                    select unit;
-
-            public Producer<RT, TResult, Unit> AsProducer<RT>(TQuery query)
-                where RT : struct, HasSqlDatabase<RT>
-                =>
-                    from result in AsAff<RT>(query)
-                    from _ in Proxy.yield(result)
-                    select unit;
-
-            public Consumer<RT, TQuery, TResult> AsConsumer<RT>()
-                where RT : struct, HasSqlDatabase<RT>
-                =>
-                    from input in Proxy.awaiting<TQuery>()
-                    from result in AsAff<RT>(input)
-                    select result;
+            public abstract Aff<RT, TResult> AsAff<RT>() where RT : struct, HasSqlDatabase<RT>;
         }
     }
 }
