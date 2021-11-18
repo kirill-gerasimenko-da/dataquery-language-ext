73051b6c76314e8691fa3eb9f83f21afed0dc319
kir <kir@your-mail.com>
2021-11-18 09:17:45 +0100

    Switch to paket, remove DI registration and dependencies

--- src/DataQuery.LanguageExt/Sql/Registrations.cs
+++ src/DataQuery.LanguageExt/Sql/Registrations.cs
@@ -1,8 +1,6 @@
 using System;
 using System.Data.SqlClient;
-using Microsoft.Extensions.DependencyInjection;
 using Npgsql;
-using SimpleInjector;
 
 namespace DataQuery.LanguageExt.Sql
 {
@@ -14,49 +12,11 @@ namespace DataQuery.LanguageExt.Sql
             Npgsql = 2
         }
 
-        /// <summary>
-        /// Registers sql database abstraction as singleton. Depending on driver type
-        /// it would use PostgreSQL SQL driver or ADO NET.  
-        /// </summary>
-        public static void AddSqlDatabase(
-            this IServiceCollection services,
+        public static ISqlDatabase CreateSqlDatabase(
             string connectionString,
             DriverType driverType = DriverType.Npgsql)
-        {
-            if (connectionString == null) throw new ArgumentNullException(nameof(connectionString));
-
-            services.AddSingleton<ISqlQueryRunner<SqlDatabaseRuntime>, SqlQueryRunner<SqlDatabaseRuntime>>(_ =>
-                new SqlQueryRunner<SqlDatabaseRuntime>(
-                    toConnection(driverType, connectionString), SqlDatabaseRuntime.New));
-
-            services.AddSingleton<ISqlDatabase, SqlDatabase>();
-        }
-
-        /// <summary>
-        /// Registers sql database abstraction as singleton. Depending on driver type
-        /// it would use PostgreSQL SQL driver or ADO NET.  
-        /// </summary>
-        public static void RegisterSqlDatabase(
-            this Container container,
-            string connectionString,
-            DriverType driverType = DriverType.Npgsql)
-        {
-            if (connectionString == null) throw new ArgumentNullException(nameof(connectionString));
-
-            container.RegisterSingleton<ISqlQueryRunner<SqlDatabaseRuntime>>(() =>
-                new SqlQueryRunner<SqlDatabaseRuntime>(
-                    toConnection(driverType, connectionString), SqlDatabaseRuntime.New));
-
-            container.RegisterSingleton<ISqlDatabase, SqlDatabase>();
-        }
-
-        /// <summary>
-        /// Factory of SQL database, if one doesn't have|need DI container
-        /// </summary>
-        public static ISqlDatabase
-            CreateSqlDatabase(string connectionString, DriverType driverType = DriverType.Npgsql) =>
-            new SqlDatabase(
-                new SqlQueryRunner<SqlDatabaseRuntime>(
+            =>
+                new SqlDatabase(new SqlQueryRunner<SqlDatabaseRuntime>(
                     toConnection(driverType, connectionString),
                     SqlDatabaseRuntime.New));
 
