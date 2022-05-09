b975820f2d522b7a513c5172c63aa4977c9e9da5
Kirill Gerasimenko <kirill.gerasimenko@dataart.com>
2022-05-09 21:54:06 +0200

    Minor rename

--- src/DataQuery.LanguageExt.Sql/SqlDb.DefaultRT.cs
+++ src/DataQuery.LanguageExt.Sql/SqlDb.DefaultRT.cs
@@ -7,60 +7,60 @@ public static partial class DataQuerySql
 {
     public static class SqlDb
     {
-        public static Aff<DefaultRT, IEnumerable<T>> query<T>(
+        public static Aff<DefaultRT, IEnumerable<T>> Query<T>(
             string sql, object param = null, int? cmdTimeout = null, CommandType? cmdType = null)
             =>
                 SqlDb<DefaultRT>.query<T>(sql, param, cmdTimeout, cmdType);
 
-        public static Aff<DefaultRT, Seq<T>> queryAll<T>(
+        public static Aff<DefaultRT, Seq<T>> QueryAll<T>(
             string sql, object param = null, int? cmdTimeout = null, CommandType? cmdType = null)
             =>
                 SqlDb<DefaultRT>.queryAll<T>(sql, param, cmdTimeout, cmdType);
 
-        public static Aff<DefaultRT, T> queryFirst<T>(
+        public static Aff<DefaultRT, T> QueryFirst<T>(
             string sql, object param = null, int? cmdTimeout = null, CommandType? cmdType = null)
             =>
                 SqlDb<DefaultRT>.queryFirst<T>(sql, param, cmdTimeout, cmdType);
 
-        public static Aff<DefaultRT, T> querySingle<T>(
+        public static Aff<DefaultRT, T> QuerySingle<T>(
             string sql, object param = null, int? cmdTimeout = null, CommandType? cmdType = null)
             =>
                 SqlDb<DefaultRT>.querySingle<T>(sql, param, cmdTimeout, cmdType);
 
-        public static Aff<DefaultRT, Option<T>> tryQueryFirst<T>(
+        public static Aff<DefaultRT, Option<T>> TryQueryFirst<T>(
             string sql, object param = null, int? cmdTimeout = null, CommandType? cmdType = null)
             =>
                 SqlDb<DefaultRT>.tryQueryFirst<T>(sql, param, cmdTimeout, cmdType);
 
-        public static Aff<DefaultRT, Option<T>> tryQuerySingle<T>(
+        public static Aff<DefaultRT, Option<T>> TryQuerySingle<T>(
             string sql, object param = null, int? cmdTimeout = null, CommandType? cmdType = null)
             =>
                 SqlDb<DefaultRT>.tryQuerySingle<T>(sql, param, cmdTimeout, cmdType);
 
-        public static Aff<DefaultRT, ISqlGridReader> queryMultiple(
+        public static Aff<DefaultRT, ISqlGridReader> QueryMultiple(
             string sql, object param = null, int? cmdTimeout = null, CommandType? cmdType = null)
             =>
                 SqlDb<DefaultRT>.queryMultiple(sql, param, cmdTimeout, cmdType);
 
-        public static Aff<DefaultRT, int> execute(
+        public static Aff<DefaultRT, int> Execute(
             string sql, object param = null, int? cmdTimeout = null, CommandType? cmdType = null)
             =>
                 SqlDb<DefaultRT>.execute(sql, param, cmdTimeout, cmdType);
 
-        public static Aff<DefaultRT, T> executeScalar<T>(
+        public static Aff<DefaultRT, T> ExecuteScalar<T>(
             string sql, object param = null, int? cmdTimeout = null, CommandType? cmdType = null)
             =>
                 SqlDb<DefaultRT>.executeScalar<T>(sql, param, cmdTimeout, cmdType);
 
-        public static Aff<DefaultRT, IDataReader> executeReader(
+        public static Aff<DefaultRT, IDataReader> ExecuteReader(
             string sql, object param = null, int? cmdTimeout = null, CommandType? cmdType = null)
             =>
                 SqlDb<DefaultRT>.executeReader(sql, param, cmdTimeout, cmdType);
     }
 
-    public static Eff<DefaultRT, IDbConnection> connection() =>
+    public static Eff<DefaultRT, IDbConnection> Connection() =>
         Eff<DefaultRT, IDbConnection>(rt => rt.Connection);
 
-    public static Eff<DefaultRT, IDbTransaction> transaction() =>
+    public static Eff<DefaultRT, IDbTransaction> Transaction() =>
         Eff<DefaultRT, IDbTransaction>(rt => rt.Transaction.IfNoneUnsafe((IDbTransaction)null));
 }
