220ae90a54bba1e591ba4dac3d0ffcbc76dc3e29
kir <kirill-gerasimenko-da@users.noreply.github.com>
2023-09-21 21:04:17 +0200

    change target framework

--- publish.sh
+++ publish.sh
@@ -2,3 +2,9 @@
 
 dotnet build ./src/DataQuery.LanguageExt.Sql/DataQuery.LanguageExt.Sql.csproj -c Release
 dotnet pack ./src/DataQuery.LanguageExt.Sql/DataQuery.LanguageExt.Sql.csproj -c Release -o ./publish /p:PackageVersion=0.0.41
+
+dotnet build ./src/DataQuery.LanguageExt/DataQuery.LanguageExt.csproj -c Release
+dotnet pack ./src/DataQuery.LanguageExt/DataQuery.LanguageExt.csproj -c Release -o ./publish /p:PackageVersion=0.0.2
+
+dotnet build ./src/DataQuery.LanguageExt.SourceGenerator/DataQuery.LanguageExt.SourceGenerator.csproj -c Release
+dotnet pack ./src/DataQuery.LanguageExt.SourceGenerator/DataQuery.LanguageExt.SourceGenerator.csproj -c Release -o ./publish /p:PackageVersion=0.0.2
