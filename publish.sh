b7de16f9f3f5e9e7db80800011295e22d165d494
kir <kirill-gerasimenko-da@users.noreply.github.com>
2023-09-22 10:26:55 +0200

    change names of the packages

--- publish.sh
+++ publish.sh
@@ -4,7 +4,13 @@ dotnet build ./src/DataQuery.LanguageExt.Sql/DataQuery.LanguageExt.Sql.csproj -c
 dotnet pack ./src/DataQuery.LanguageExt.Sql/DataQuery.LanguageExt.Sql.csproj -c Release -o ./publish /p:PackageVersion=0.0.41
 
 dotnet build ./src/DataQuery.LanguageExt/DataQuery.LanguageExt.csproj -c Release
-dotnet pack ./src/DataQuery.LanguageExt/DataQuery.LanguageExt.csproj -c Release -o ./publish /p:PackageVersion=0.0.3
+dotnet pack ./src/DataQuery.LanguageExt/DataQuery.LanguageExt.csproj -c Release -o ./publish /p:PackageVersion=0.0.4
 
-dotnet build ./src/DataQuery.LanguageExt.SourceGenerator/DataQuery.LanguageExt.SourceGenerator.csproj -c Release
-dotnet pack ./src/DataQuery.LanguageExt.SourceGenerator/DataQuery.LanguageExt.SourceGenerator.csproj -c Release -o ./publish /p:PackageVersion=0.0.3
+dotnet build ./src/DataQuery.LanguageExt.SourceGenerator.Common/DataQuery.LanguageExt.SourceGenerator.Common.csproj -c Release
+dotnet pack ./src/DataQuery.LanguageExt.SourceGenerator.Common/DataQuery.LanguageExt.SourceGenerator.Common.csproj -c Release -o ./publish /p:PackageVersion=0.0.4
+
+dotnet build ./src/DataQuery.LanguageExt.SourceGenerator.NormNet/DataQuery.LanguageExt.SourceGenerator.NormNet.csproj -c Release
+dotnet pack ./src/DataQuery.LanguageExt.SourceGenerator.NormNet/DataQuery.LanguageExt.SourceGenerator.NormNet.csproj -c Release -o ./publish /p:PackageVersion=0.0.4
+
+dotnet build ./src/DataQuery.LanguageExt.SourceGenerator.SystemData/DataQuery.LanguageExt.SourceGenerator.SystemData.csproj -c Release
+dotnet pack ./src/DataQuery.LanguageExt.SourceGenerator.SystemData/DataQuery.LanguageExt.SourceGenerator.SystemData.csproj -c Release -o ./publish /p:PackageVersion=0.0.4
