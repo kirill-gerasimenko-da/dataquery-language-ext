09ac50115de17a0c3d17c6060e65464f67dc2c23
kir <kirill-gerasimenko-da@users.noreply.github.com>
2023-12-02 15:06:20 +0100

    updates

--- publish.sh
+++ publish.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 dotnet build ./src/DataQuery.LanguageExt.Sql/DataQuery.LanguageExt.Sql.csproj -c Release
-dotnet pack ./src/DataQuery.LanguageExt.Sql/DataQuery.LanguageExt.Sql.csproj -c Release -o ./publish /p:PackageVersion=0.0.41
+dotnet pack ./src/DataQuery.LanguageExt.Sql/DataQuery.LanguageExt.Sql.csproj -c Release -o ./publish /p:PackageVersion=0.0.42
 
-VER=0.0.8
+VER=0.0.10
 
 dotnet build ./src/DataQuery.LanguageExt/DataQuery.LanguageExt.csproj -c Release
 dotnet pack ./src/DataQuery.LanguageExt/DataQuery.LanguageExt.csproj -c Release -o ./publish /p:PackageVersion=$VER
